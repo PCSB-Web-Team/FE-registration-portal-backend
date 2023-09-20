138a7c0b5a950b0f0d0f44fcf7e1b600e4165890
AbhishekJadhav2002 <www.abhishek3jadhav@gmail.com>
2023-09-20 23:32:05 +0530

    feat: added admin authorization and get user by id route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { User, validateUser } = require("./models/userSchema");
-const connectDatabase = require("./config/db");
 const cors = require("cors");
+const { ADMIN_TOKEN } = require("./config");
 
 const app = express();
 app.use(cors());
@@ -9,7 +9,7 @@ app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello from Drive API server");
+  res.send("Hello from PCSB Registration Portal API server");
 });
 
 app.post("/register", async (req, res) => {
@@ -33,14 +33,19 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.get("/users", async (req, res) => {
+app.get("/users/:token", async (req, res) => {
+  const token = req.params.token;
+  if (!token || token !== ADMIN_TOKEN) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
   const page = parseInt(req.query.page) || 1; // Default to page 1
   const pageSize = parseInt(req.query.pageSize) || 10; // Default page size
 
   try {
     const skip = (page - 1) * pageSize;
     const totalUsers = await User.countDocuments(); // Get the total number of users
-    const users = await User.find().skip(skip).limit(pageSize);
+    const users = await User.find({}, "-receiptImage").skip(skip).limit(pageSize);
 
     const totalPages = Math.ceil(totalUsers / pageSize);
 
@@ -55,6 +60,25 @@ app.get("/users", async (req, res) => {
     });
   } catch (error) {
     console.error("Error retrieving paginated users:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/users/:id/:token", async (req, res) => {
+  if (!req.params.token || req.params.token !== ADMIN_TOKEN) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error retrieving user:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
