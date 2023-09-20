37d1f61528480a3f7c19d156a69496313a259fba
AbhishekJadhav2002 <www.abhishek3jadhav@gmail.com>
2023-09-20 23:31:26 +0530

    feat: added admin token env config

--- config/index.js
+++ config/index.js
@@ -4,5 +4,6 @@ dotenv.config();
 
 const DB_URI = process.env.DB_URI;
 const APP_PORT = process.env.APP_PORT;
+const ADMIN_TOKEN = process.env.ADMIN_TOKEN;
 
-module.exports = { DB_URI, APP_PORT }
+module.exports = { DB_URI, APP_PORT, ADMIN_TOKEN }
